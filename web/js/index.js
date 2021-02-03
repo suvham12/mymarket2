0730eaaad1fb8b15123962128d95b17ee0fc4d3e
suvhamdas12 <60747945+suvhamdas12@users.noreply.github.com>
2021-02-03 16:36:47 +0530

    yes
    well

---
+++ web/js/index.js
@@ -0,0 +1,6 @@
+function login()
+{
+	var usermail=document.getElementById("email").value;
+	var userpass=document.getElementById("password").value;
+	window.alert("ur user name is:\n"+usermail+"ur password is:"+userpass);
+}
